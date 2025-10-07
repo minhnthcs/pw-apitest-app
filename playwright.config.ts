d31b25cdb8ab6c07b1ab312f75ebdbf4a66de439
mason <minh.nthcs@gmail.com>
2025-10-07 08:05:57 +0700

    sharing authenticate state

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
+import path from "path";
 
 /**
  * Read environment variables from file.
@@ -12,7 +13,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -22,52 +23,46 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "setup",
+      testMatch: "auth.setup.ts", // Run only once
     },
-
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+        storageState: path.resolve(__dirname, "./.auth/user.json"),
+      },
+      dependencies: ["setup"], // 🔥 Ensures setup runs before
     },
-
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: "firefox",
+      use: {
+        ...devices["Desktop Firefox"],
+        storageState: path.resolve(__dirname, "./.auth/user.json"),
+      },
+      dependencies: ["setup"],
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    {
+      name: "webkit",
+      use: {
+        ...devices["Desktop Safari"],
+        storageState: path.resolve(__dirname, "./.auth/user.json"),
+      },
+      dependencies: ["setup"],
+    },
   ],
 
   /* Run your local dev server before starting the tests */
--- tests/workingWithApi.spec.ts
+++ tests/workingWithApi.spec.ts
@@ -1,21 +1,28 @@
 import { test, expect } from "@playwright/test";
+import fs from "fs";
+const { token } = JSON.parse(fs.readFileSync("./.auth/token.json", "utf-8"));
+
 import tags from "../test-data/tags.json";
 
 test.beforeEach(async ({ page }) => {
-  await page.route("**/api/tags*", async (route) => {
-    await route.fulfill({
-      status: 200,
-      contentType: "application/json",
-      body: JSON.stringify(tags),
-    });
-    console.log("Intercepted:", route.request().url());
-  });
+  // await page.route("**/api/tags*", async (route) => {
+  //   await route.fulfill({
+  //     status: 200,
+  //     contentType: "application/json",
+  //     body: JSON.stringify(tags),
+  //   });
+  //   console.log("Intercepted:", route.request().url());
+  // });
 
+  await page.goto("https://conduit.bondaracademy.com/");
+});
+
+test("has title", async ({ page }) => {
   await page.route("**/api/articles*", async (route) => {
     const response = await route.fetch();
     const responseBody = await response.json();
-    responseBody.articles[0].title = "This is a test title";
-    responseBody.articles[0].description = "This is a test description";
+    responseBody.articles[0].title = "This is a MOCK test title";
+    responseBody.articles[0].description = "This is a MOCK test description";
 
     await route.fulfill({
       status: 200,
@@ -24,27 +31,69 @@ test.beforeEach(async ({ page }) => {
     });
   });
 
-  page.on("request", (req) => console.log("Outgoing:", req.url()));
-  await page.goto("https://conduit.bondaracademy.com/");
-});
+  await page.getByText("Global Feed").click();
 
-test("has title", async ({ page }) => {
   await page.waitForTimeout(1000);
   // trigger the API call - this very important to make sure the api call to be mocked
   await page.waitForSelector(".tag-list");
 
   // assert that mocked data appeared - this very important to make sure the api call to be mocked
-  for (const tag of tags.tags) {
-    await expect(page.locator(".tag-list")).toContainText(tag);
-  }
+  // for (const tag of tags.tags) {
+  //   await expect(page.locator(".tag-list")).toContainText(tag);
+  // }
   // Expect a title "to contain" a substring.
   await expect(page.locator(".navbar-brand")).toHaveText("conduit");
 
+  // this is having a bug of playwright, need a assertion to modify the api successfully
   await expect(page.locator("app-article-list h1").first()).toContainText(
-    "This is a test title"
+    "This is a MOCK test title"
   );
   await expect(page.locator("app-article-list p").first()).toContainText(
-    "This is a test description"
+    "This is a MOCK test description"
+  );
+});
+
+test("delete articles", async ({ request, page }) => {
+  console.log("token", token);
+
+  const articleResponse = await request.post(
+    "https://conduit-api.bondaracademy.com/api/articles/",
+    {
+      data: {
+        article: {
+          title: "This is my test Mason ",
+          description: "This is test about",
+          body: "This is test description",
+          tagList: [],
+        },
+      },
+      headers: {
+        Authorization: `Token ${token}`,
+      },
+    }
+  );
+
+  expect(articleResponse.status()).toEqual(201);
+  const articleSlug = (await articleResponse.json()).article.slug;
+
+  await page.waitForTimeout(1000);
+  await page.getByText("Global Feed").click();
+  expect(await page.locator("app-article-list h1").first()).toContainText(
+    "This is my test Mason"
+  );
+
+  const deleteResponse = await request.delete(
+    `https://conduit-api.bondaracademy.com/api/articles/${articleSlug}`,
+    {
+      headers: {
+        Authorization: `Token ${token}`,
+      },
+    }
+  );
+  expect(deleteResponse.status()).toEqual(204);
+  await page.getByText("Global Feed").click();
+  expect(await page.locator("app-article-list h1").first()).not.toContainText(
+    "This is my test Mason"
   );
 });
 
